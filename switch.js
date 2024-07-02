946a1517bc2c77105f6c5d0c96204631f917bd67
Dennis <dennisjean0226@gmail.com>
2024-07-02 17:36:58 +0800

    i think ill add do while loop

---
+++ switch.js
@@ -0,0 +1,22 @@
+let init = 0;
+let person = "name";
+let kok = "neger"
+function as ()
+{console.log("you are an ass")
+
+}
+init++ 
+console.log(init);
+switch(person || kok)
+{
+    case "name":
+        console.log("hey man");
+        as()
+        console.log("i miss you")
+        break;
+    case "neger":
+        console.log("hey dude");
+        break;
+    default:
+        console.log("you are not a person");
+}
--- tictactoe.js
+++ tictactoe.js
@@ -2,7 +2,8 @@
 // oh I see like you can choose a column or something
 // like input what column you want and choose if x or o
 //so get's two user input 
-
+// do while maybe ????? 
+// yeah could be
 const readline = require("readline");
 
 const r2 = readline.createInterface({
@@ -21,19 +22,7 @@ return new Promise((resolve)=>
 
 //main()
 
-person2(); 
-
-let aso = "";
-let aso2= "";
-let dif = "";
-let ale =false;
-
-
-const array= [["a","b","c"],["d","e","f"],["g","h","i"]]
-   async function person2()
-   {
-
-    console.log("CHOOSE NUMBER (1-9)");
+console.log("CHOOSE NUMBER (1-9)");
 
     console.log("1.upleft");
     console.log("2.upmidle");
@@ -44,11 +33,42 @@ const array= [["a","b","c"],["d","e","f"],["g","h","i"]]
     console.log("7.bottomleft");
     console.log("8.bottommiddle");
     console.log("9.bottomright");
-    
-    const    aw = await getInput("player 1: ");
-     
-        
+
+let aso = "";
+let aso2= "";
+let dif = "";
+let ale =true;
+let ale2= false;
+let init = 1 ;
+let awz  =""
+let play2z = ""
+const array= [ [" "," "," "],[" "," "," "],[" "," "," "]]
+   async function person2()
+   {   
+    let ale =true;
+    if(init % 2 !== 0)
+      {
+   awz = await getInput("player 1: ");//odd
+
+      }
+  else{
+    play2z = await getInput('player 2: ')//even // make this a function may be idk really
+
   
+  }
+  console.log("value init" + init);
+   // async function ss()
+    // {
+     // if(ale2 === true)
+       // {
+          
+       // }
+      
+
+   //  }
+ 
+  let aw = awz;
+  let play2 = play2z;
            //saq = await getInput('player 2: ')
     
 
@@ -57,165 +77,354 @@ const array= [["a","b","c"],["d","e","f"],["g","h","i"]]
         //  {
          //   const as =await getInput("player 2:")
         //  }
-       
+       // good after noon sir im gonna rape you 
 
-  
- 
 
-   
+
+
+
+
+
     
-    switch(aw)
+    switch(aw || play2)
     {
     case "1":
-    array[0][0] ="*"
-    console.log(array)
-for(let i= 0; i < 3;i++)
-{
-  for(let j = 0; j< 3;j++)
-    {
-  
-array[i][j] +=``
-
     
-    }
-
  
-}
-console.log(array);
-ale=true
-person2()
+//array.forEach(subA => {
+ // console.log(array[0][0] + " s " + array[0][1]);
+ 
+//})
 
+console.log(dif + " are you deaf ma men");
+//odd even number wait this is fun
+//function checkNull()
+//{
+
+//}
+//checkNull()
+function prinTable()
+{
+ 
+ console.log("value init" + init);
+  console.log(
+     `    [=============]
+    :| ${array[0][0]} | ${array[0][1]} | ${array[0][2]} |:
+     -------------
+    :| ${array[1][0]} | ${array[1][1]} | ${array[1][2]} |:
+     ------------- 
+    :| ${array[2][0]} | ${array[2][1]} | ${array[2][2]} |:
+    [=============]
+    `
+    )
+
+}
+if(array[0][0] !== " ")
+  {
    
+    console.log(" Occupied slot . Try again")
+    console.log(init+ " value if this the heck")
+    
+    person2();
+
+
+    aw = "";
+    play2 ="";
+   return;
+  }
+  else{
+    if(init % 2 == 0  )
+      {
+       
+        array[0][0] ="O"
+       
+        init++;
+        prinTable()
+        person2()
+      
+        console.log("sssssssssssssssssssssssssssssssssssssssssssss")
+        aw= ""
+        play2= ""
+        ale = false;
+      }
+     
+      else if (init % 2 !== 0 ){
+    array[0][0] ="X"
+    prinTable()
+    init++
+    ale = false;
+
+    person2()
+    break;
+        aw =""
+    play2 = ""
+      }
+  }
+
+
+  
+
+
+
   
     break;
     case "2":
-     array[0][1] = "*"
-     for(let i= 0; i < 3;i++)
-{
-  for(let j = 0; j< 3;j++)
-    {
-    
-array[i][j] +=``
+      if(array[0][1] === "X" || array[0][1] === "O")
+        {
+          console.log("Occupied slot. Try again")
+          console.log(init+ " value if this the heck----------------------")
+          prinTable()
+          person2();
+         
+         
+        }
+      else if(init % 2 == 0  && ale === true)
+        {
+         
+          array[0][1] ="O"
+          prinTable()
+          init++;
+         ale = false
+          person2()
+        }
+       
+        else if (init % 2 !== 0 && ale === true){
+   array[0][1] = "X"
+      prinTable()
+      init++
+      ale =false 
       
+      person2()
+      
+        }
+  
     
-    }
-
- 
-}
-console.log(array);
-
-ale=true
-person2()
+   
    
      break;
      case "3":
-       array[0][2] = "*"
-       for(let i= 0; i < 3;i++)
+      
+      
+     if(array[0][1] === "X" || array[0][1] === "O")
+      {
+        console.log("Occupied slot. Try again")
+        person2();
+       
+       
+      }
+
+      if(init % 2 == 0 )
         {
-          for(let j = 0; j< 3;j++)
-            {
-           
-        array[i][j] +=``
-             
-            
-            }
-        
+         
+          array[0][2] ="O"
+          prinTable()
+          init++;
+         
+          person2()
+        }
+       
+        else{
+      array[0][2] ="X"
+      prinTable()
+      init++
+      
+     
+      person2()
+      
+        }
+      
+      
+     
+     break;
+    case "4":
+      if(array[1][0] === "X" || array[1][0] === "O")
+        {
+          console.log("Occupied slot. Try again")
+          person2();
+         
          
         }
-        console.log(array);
-        
-        ale=true
-        person2()
-           
-     break
-    case "4":
-    array[1][0] = "*" 
-    for(let i= 0; i < 3;i++)
-      {
-        for(let j = 0; j< 3;j++)
-          {
-          
-      array[i][j] +=``
-        
-          
-          }
-      
-       
-      }
-      console.log(array);
-      
-      ale=true
-      person2()
+      if(init % 2 == 0 )
+        {
          
-    break
+          array[1][0] ="O"
+          prinTable()
+          init++;
+         
+          person2()
+        }
+       
+        else{
+      array[1][0] ="X"
+      prinTable()
+      init++
+      
+     
+      person2()
+      
+        }
+ 
+         
+    break;
     case "5":
-     array[1][1]  = "*"
-     for(let i= 0; i < 3;i++)
-      {
-        for(let j = 0; j< 3;j++)
-          {
-        
-      array[i][j] +=``
-           
-          
-          }
-      
-       
-      }
-      console.log(array);
-      
-      ale=true
-      person2()
+      if(array[1][1] === "X" || array[1][1] === "O")
+        {
+          console.log("Occupied slot. Try again")
+          person2();
          
-     break
+         
+        }
+      if(init % 2 == 0 )
+        {
+         
+          array[1][1] ="O"
+          prinTable()
+          init++;
+         
+          person2()
+        }
+       
+        else{
+      array[1][1] ="X"
+      prinTable()
+      init++
+      
+     
+      person2()
+      
+        }
+    
+         
+     break;
      case "6":
-       array[1][2] + "*"
+      if(array[1][2] === "X" || array[1][2] === "O")
+        {
+          console.log("Occupied slot. Try again")
+          person2();
+         
+         
+        }
+      if(init % 2 == 0 )
+        {
+         
+          array[1][2] ="O"
+          prinTable()
+          init++;
+          
+          person2()
+        }
+       
+        else{
+      array[1][2] ="X"
+      prinTable()
+      init++
+      
+     
+      person2()
+      
+        }
+
+
        break;
        case "7":
-         array[2][0] = "*"
-       break
-    case "8":
-     array[2][1] = "*"
-     for(let i= 0; i < 3;i++)
-      {
-        for(let j = 0; j< 3;j++)
+        if(array[2][0] === "X" || array[2][0] === "O")
           {
+            console.log("Occupied slot. Try again")
+            person2();
+           
+           
+          }
+        if(init % 2 == 0 )
+          {
+           
+            array[2][0] ="O"
+            prinTable()
+            init++;
+            
+            person2()
+          }
+         
+          else{
+        array[2][0] ="X"
+        prinTable()
+        init++
         
-      array[i][j] +=``
-          
-          
+        ale2 = true;
+        person2()
+        
           }
       
-       
-      }
-      console.log(array);
-      
-      ale=true
-      person2()
-         
-     break
-     case "9":
-       array[2][2] = "*"
-       for(let i= 0; i < 3;i++)
+       break;
+    case "8":
+      if(array[2][1] === "X" || array[2][1] === "O")
         {
-          for(let j = 0; j< 3;j++)
-            {
-           
-        array[i][j] +=``
-            
-            }
-        
+          console.log("Occupied slot. Try again")
+          person2();
+         
          
         }
-        console.log(array);
-        
-        ale=true
-        person2()
-           
-       break
+      if(init % 2 == 0 )
+        {
+         
+          array[2][1] ="O"
+          prinTable()
+          init++;
+          ale2= true;
+          person2()
+        }
+       
+        else{
+      array[2][1] ="X"
+      prinTable()
+      init++
+      
+      ale2 = true;
+      person2()
+      
+        }
     
+
+         
+     break;
+     case "9":
+      if(array[2][2] === "X" || array[2][2] === "O")
+        {
+          console.log("Occupied slot. Try again")
+          person2();
+         
+         
+        }
+      if(init % 2 == 0 )
+        {
+         
+          array[2][2] ="O"
+          prinTable()
+          init++;
+          ale2= true;
+          person2()
+        }
+       
+        else{
+      array[2][2] ="X"
+      prinTable()
+      init++
+      
+      ale2 = true;
+      person2()
+      
+        }
+    
+
+     
+           
+       break;
+ 
+       
+
     }
    
+  
+  }
 
 
 
@@ -231,6 +440,7 @@ person2()
 
 
 
-   }
 
-   function smart (){}
+   
+person2(); 
+console.log("know what the fuck ")
