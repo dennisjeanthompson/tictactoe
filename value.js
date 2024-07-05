9f15ccfd16aa942f69136eb3b4c48897528387d8
Dennis <dennisjean0226@gmail.com>
2024-07-05 23:25:20 +0800

    im gonna sleep for know

--- value.js
+++ value.js
@@ -1,3 +1,10 @@
+let move = "3"
 const pos = parseInt(move) - 1;
 const row = Math.floor(pos / 3);
 const col = pos % 3;
+console.log(col +  "col ") 
+console.log(row + " row")
+console.log(pos + "  pos");
+const pos1 = 2;
+pos1 =Math.floor(3/3);
+console.log(pos1 ( 2 %3 )) 
